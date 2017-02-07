0048d7b9e1d62e18bab1e5b7ec5b6a22c59ab55a
callistusystan <callistusystan@gmail.com>
2017-02-07 22:48:24 +0800

    first commit

---
+++ src/components/common/index.js
@@ -0,0 +1,9 @@
+// this index file is a pattern that allows easier imports
+// however, it requires exports of each file to be named
+
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
